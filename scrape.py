6b7c5c0a3ba3ac02a91bd52de27616fd66d60e55
ArjunMehta01 <arjun.mehta1001@gmail.com>
2020-09-12 11:14:51 -0600

    improvements to scraper.py

--- scrape.py
+++ scrape.py
@@ -2,24 +2,35 @@ from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import json
 
+
+
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
 url = 'https://www.seriouseats.com/recipes/2012/08/grilled-butterflied-chicken-recipe.html'
 
-class ScrapeTool():
+class ScrapeTool(): # 
 
-    def __init__(self,url):
-        self.url = url
+    def __init__(self,urlsDic):
+        self.urlsDic = urlsDic
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
-        headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
-        req = Request(url=url,headers=headers)
-        page = urlopen(req)
+    def openJSON(self):
+        with open("") as json_file:# arbitrary file name
+            data = json.loads(json_file)#dict
 
-        self.soup = BeautifulSoup(page,'html.parser')
+        json_file.close() 
+
+        return data  
     
+    def desoup(self, url):
+        req = Request(url=url,headers=self.headers)
+        page = urlopen(req)
+        soup = BeautifulSoup(page,'html.parser')
 
-    def recipeScrape(self):
-        content = self.soup.find_all('p')
+        return soup
+    
+    def recipeScrape(self, soup):
+        content = soup.find_all('p')
         article = ""
 
         for i in content:
@@ -27,4 +38,24 @@ class ScrapeTool():
         
         return article
 
+    def createDic(self, inputDic):
+        newDic = {}
+        for keys in inputDic:
+            desouped = self.desoup(inputDic[keys])
+            scraped = self.recipeScrape(desouped)
+            newDic[keys] = scraped
+        return newDic
     
+    def exportJSON(self, data):
+        with open("", 'w') as outfile:
+            json.dump(data,outfile)
+        outfile.close()
+
+
+
+
+
+test = ScrapeTool({'test': url})
+
+print(test.createDic({'test': url, 'test2': 'https://www.seriouseats.com/recipes/2012/08/grilled-butterflied-chicken-recipe.html'}))
+
