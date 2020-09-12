42a5c4ae31845788e6d7837e5db097fe27e4dc92
ArjunMehta01 <arjun.mehta1001@gmail.com>
2020-09-12 14:55:40 -0600

    demo of js python script runner complete

--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 import json
+import os
+import sys
 
 
 
@@ -8,20 +10,25 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML
 
 url = 'https://www.seriouseats.com/recipes/2012/08/grilled-butterflied-chicken-recipe.html'
 
+def openJSON(JSONfile):
+    with open(JSONfile) as json_file:# arbitrary file name
+        data = json.load(json_file)#dict
+
+    json_file.close() 
+
+    return data  
+
+def exportJSON(data,JSONfile):
+    with open(JSONfile, 'w') as outfile:
+        json.dump(data,outfile)
+    outfile.close()
+
 class ScrapeTool(): # 
 
     def __init__(self,urlsDic):
         self.urlsDic = urlsDic
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
-    def openJSON(self):
-        with open("") as json_file:# arbitrary file name
-            data = json.loads(json_file)#dict
-
-        json_file.close() 
-
-        return data  
-    
     def desoup(self, url):
         req = Request(url=url,headers=self.headers)
         page = urlopen(req)
@@ -45,17 +52,14 @@ class ScrapeTool(): #
             scraped = self.recipeScrape(desouped)
             newDic[keys] = scraped
         return newDic
-    
-    def exportJSON(self, data):
-        with open("", 'w') as outfile:
-            json.dump(data,outfile)
-        outfile.close()
+
+
+if __name__ == '__main__':
+    dataIn = openJSON('testido.json')
+    recipes = ScrapeTool(dataIn)
+    dataOut = recipes.createDic(dataIn)
+    exportJSON(dataOut,'finalTest.json')
 
 
 
-
-
-test = ScrapeTool({'test': url})
-
-print(test.createDic({'test': url, 'test2': 'https://www.seriouseats.com/recipes/2012/08/grilled-butterflied-chicken-recipe.html'}))
 
